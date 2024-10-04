dd9fce34dd0fc6140ae021a86c61ee0ccc6fc82f
Kowser <107254329+KowserPy@users.noreply.github.com>
2024-10-04 21:09:52 +0600

    try to add back buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Outlet } from "react-router-dom";
 import MenuSlider from "./components/MenuSlider";
 import ProtectedRoute from "./components/ProtectedRoute";
 
 const App = () => {
-	useEffect(() => {
-		Telegram.WebApp.BackButton.show();
-	});
 	return (
 		<ProtectedRoute>
 			<Outlet />
--- src/pages/Task.jsx
+++ src/pages/Task.jsx
@@ -1,6 +1,25 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Task = () => {
+	const navigate = useNavigate();
+	useEffect(() => {
+		// Show the Telegram back button
+		Telegram.WebApp.BackButton.show();
+
+		const handleBackButtonClick = () => {
+			navigate(-1); // Go back to the previous route
+		};
+
+		// Set the click event for the Telegram back button
+		Telegram.WebApp.BackButton.onClick(handleBackButtonClick);
+
+		// Clean up the event listener and hide the back button when unmounted
+		return () => {
+			Telegram.WebApp.BackButton.offClick(handleBackButtonClick);
+			Telegram.WebApp.BackButton.hide(); // Optionally hide the button when leaving the page
+		};
+	}, [navigate]);
 	return <div>Task</div>;
 };
 
