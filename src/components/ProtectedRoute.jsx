616f6e9dd3a50e7d3aa596c723cdb8e57f1feae6
Kowser <107254329+KowserPy@users.noreply.github.com>
2024-10-04 23:17:38 +0600

    bug solve 2

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,27 +1,21 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useSelector } from "react-redux";
 import { Navigate, useLocation } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
-	useEffect(() => {
-		const location = useLocation();
-		const queryParams = new URLSearchParams(location.search);
-		console.log(location);
-		console.log(queryParams);
-	});
 	const location = useLocation();
 	const queryParams = new URLSearchParams(location.search);
 	const referralCode = queryParams.get("tgWebAppStartParam");
 	console.log("referralCode", referralCode);
 	const { isAuthenticated } = useSelector((state) => state.user);
 
-	// // If user is not logged in, redirect to /startapp
-	// if (!isAuthenticated) {
-	// 	// If referral code exists, append it to the redirect URL
-	// 	const redirectURL = referralCode ? `/startapp?reff=${referralCode}` : "/startapp";
+	// If user is not logged in, redirect to /startapp
+	if (!isAuthenticated) {
+		// If referral code exists, append it to the redirect URL
+		const redirectURL = referralCode ? `/getStarted?startapp=${referralCode}` : "/getStarted";
 
-	// 	return <Navigate to={redirectURL} />;
-	// }
+		return <Navigate to={redirectURL} />;
+	}
 	return children;
 };
 
