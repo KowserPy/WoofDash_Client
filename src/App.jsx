4ce0e23c1f036dd7ce5173eec30f98dbcc32f1b8
Kowser <107254329+KowserPy@users.noreply.github.com>
2024-10-04 21:01:37 +0600

    try to add back buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import MenuSlider from "./components/MenuSlider";
 import ProtectedRoute from "./components/ProtectedRoute";
 
 const App = () => {
+	useEffect(() => {
+		Telegram.WebApp.BackButton.show();
+	});
 	return (
 		<ProtectedRoute>
 			<Outlet />
